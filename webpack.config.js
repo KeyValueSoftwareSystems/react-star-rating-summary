6f81aade06ace725657f339af68b5d03aea3732f
manacy-keyvalue <112211103+manacy-keyvalue@users.noreply.github.com>
2023-12-01 15:38:25 +0530

    Fix: Added support for SSR, Revised license and README (#15)

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,38 @@ const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const getPackageJson = require('./scripts/getPackageJson');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-const {
-  version,
-  name,
-  license,
-  repository,
-  author,
-} = getPackageJson('version', 'name', 'license', 'repository', 'author');
+const { version, name, license, repository, author } = getPackageJson(
+  'version',
+  'name',
+  'license',
+  'repository',
+  'author'
+);
 
 const banner = `
   ${name} v${version}
   ${repository.url}
 
-  Copyright (c) ${author.replace(/ *<[^)]*> */g, " ")} and project contributors.
+  Copyright (c) ${author.replace(/ *<[^)]*> */g, ' ')} and project contributors.
 
   This source code is licensed under the ${license} license found in the
   LICENSE file in the root directory of this source tree.
 `;
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   devtool: 'source-map',
   entry: './src/index.tsx',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
-    library: "MyLibrary",
+    library: 'React Star Rating Summary',
     libraryTarget: 'umd',
-    clean: true
+    clean: true,
+    globalObject: 'this'
   },
   externals: {
     react: 'react'
@@ -42,7 +43,7 @@ module.exports = {
     minimizer: [
       new TerserPlugin({ extractComments: false }),
       new CssMinimizerPlugin()
-    ],
+    ]
   },
   module: {
     rules: [
@@ -78,6 +79,6 @@ module.exports = {
     new webpack.BannerPlugin(banner)
   ],
   resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.json', ".css", ".scss"]
+    extensions: ['.ts', '.js', '.tsx', '.json', '.css', '.scss']
   }
 };
